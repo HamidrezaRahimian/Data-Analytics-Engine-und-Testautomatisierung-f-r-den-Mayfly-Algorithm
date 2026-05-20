623a2e78ec3713e6ce6c541655fab0b3e4507a0c
hamidrezarahimian15 <hamidrezarahimian15@gmail.com>
2026-05-20 13:20:50 +0200

    Convergernce analyzer tests added

--- CarstenAAAAAAAAAAAAAAAAAAAAAAA/src/test/java/edu/swarmintelligence/mayfly/ConvergenceAnalyzerTest.java
+++ CarstenAAAAAAAAAAAAAAAAAAAAAAA/src/test/java/edu/swarmintelligence/mayfly/ConvergenceAnalyzerTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
@@ -107,10 +108,10 @@ class ConvergenceAnalyzerTest {
         }
     }
 
-    static Stream<org.junit.jupiter.params.provider.Arguments> plateauParameters() {
+    static Stream<Arguments> plateauParameters() {
         return Stream.of(
-                org.junit.jupiter.params.provider.Arguments.of(0.0, 4),
-                org.junit.jupiter.params.provider.Arguments.of(0.01, 3)
+                Arguments.of(0.0, 4),
+                Arguments.of(0.01, 3)
         );
     }
 
