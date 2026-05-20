913f611e14a00eb09d410bbca034382e12e8fb84
hamidrezarahimian15 <hamidrezarahimian15@gmail.com>
2026-05-20 21:34:45 +0200

    first -giventest

---
+++ CarstenAAAAAAAAAAAAAAAAAAAAAAA/src/test/java/edu/swarmintelligence/mayfly/GivenMayflyConfiguration.java
@@ -0,0 +1,61 @@
+package edu.swarmintelligence.mayfly;
+
+import com.tngtech.jgiven.Stage;
+import com.tngtech.jgiven.annotation.ProvidedScenarioState;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GivenMayflyConfiguration extends Stage<GivenMayflyConfiguration> {
+    @ProvidedScenarioState
+    MayflyConfig config;
+
+    @ProvidedScenarioState
+    long seed;
+
+    @ProvidedScenarioState
+    long secondSeed;
+
+    @ProvidedScenarioState
+    List<Long> seeds;
+
+    @ProvidedScenarioState
+    double threshold;
+
+    @ProvidedScenarioState
+    ConvergenceAnalyzer convergenceAnalyzer;
+
+    public GivenMayflyConfiguration the_default_configuration() {
+        config = MayflyConfig.ackley10D();
+        return self();
+    }
+
+    public GivenMayflyConfiguration seed_$_is_used(long seed) {
+        this.seed = seed;
+        return self();
+    }
+
+    public GivenMayflyConfiguration the_same_seed_is_used_twice(long seed) {
+        this.seed = seed;
+        this.secondSeed = seed;
+        return self();
+    }
+
+    public GivenMayflyConfiguration the_female_attraction_threshold_is(double threshold) {
+        this.threshold = threshold;
+        return self();
+    }
+
+    public GivenMayflyConfiguration ten_fixed_seeds_are_used() {
+        seeds = new ArrayList<>();
+        for (long i = 1; i <= 10; i++) {
+            seeds.add(i);
+        }
+        return self();
+    }
+
+    public GivenMayflyConfiguration a_constant_fitness_stream() {
+        convergenceAnalyzer = new ConvergenceAnalyzer(0.001, 0.0, 5);
+        return self();
+    }
+}
